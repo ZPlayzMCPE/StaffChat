10ac08847842f7461ab9cfeace219e605921986f
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-01-11 11:06:51 +0000

    Fixed /sc help.

--- src/Thunder33345/StaffChat/StaffChat.php
+++ src/Thunder33345/StaffChat/StaffChat.php
@@ -142,15 +142,15 @@ class StaffChat extends PluginBase implements Listener
     if(!isset($args[0])) $args[0] = "help";
     switch($args[0]){
       case "help":
-        $msgs = ['§5Page','§b/say <message> §3- chat into staff chat','§b/on §3- enable Staff chatting mode','§b/off §3- disable Staff chatting mode','§btoggle §3- toggle chatting mode',//'config - config command',//maybe latter...
-         '§b/reload §3- reloads and flushes internal data','§b/attach <true|false> §3- attach console into staff chat','§b/check <player> §3- checks other player status',//'setl [-fs] <player> <true|false> - sets other players listen status',
+        $msgs = ['§aStaff§2Chat §dHelp §5Page','§bsay <message> §3- chat into staff chat','§bon §3- enable Staff chatting mode','§boff §3- disable Staff chatting mode','§btoggle §3- toggle chatting mode',//'config - config command',//maybe latter...
+         '§breload §3- reloads and flushes internal data','§battach <true|false> §3- attach console into staff chat','§bcheck <player> §3- checks other player status',//'setl [-fs] <player> <true|false> - sets other players listen status',
           //'setc [-fs] <player> <true|false> - sets other players chat status',
           //'-f to forcefully set player status regardless of their permission',
           //'-s silently sets staffchat status',
           //'-n dont notify other staff(only for players with insufficient permissions)',
-         'author - show author info',//' - ',
+         '§bauthor §3- show author info',//' - ',
         ];
-        foreach($msgs as $msg) $sender->sendMessage(TextFormat::GOLD."§aStaff §2Chat §dHelp ".$msg);
+        foreach($msgs as $msg) $sender->sendMessage(TextFormat::GOLD."§b/sc ".$msg);
         return true;
         break;
       case "say":
